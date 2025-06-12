3c83d52ed4a2190b2938761d45d3c42d36a320a9
James-Ian09 <jamesianbayonas21@gmail.com>
2025-06-12 14:06:28 +0800

    added rest api for implementation for the back end

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
-import express from "express";
+import express from "express"; // imports the framework:: (express.js) and take note that express is a function
 
-const app = express();
+const app = express(); // created an instance of express
+
+app.get("/api/notes", (req, res) => {
+  res.status(200).send("you got notes and I don't");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Note Created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Notes have been successfully updated!" });
+});
+
+app.delete("/api/notes", (req, res) => {
+  res.status(200).send({ message: "Notes have been successfully deleted" });
+});
 
 app.listen(5001, () => {
   console.log("Server started on PORT:: 5001");
