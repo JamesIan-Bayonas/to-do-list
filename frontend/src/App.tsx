c74f9cdb3abada97eaeb3941c136458a40566c4c
James-Ian09 <jamesianbayonas21@gmail.com>
2025-06-30 23:13:34 +0800

    added homepage, createpage, and notedetails:: for users notetaking

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router";
+
+import Homepage from "./pages/Homepage";
+import NoteDetailpage from "./pages/NoteDetailpage";
+import Createpage from ".page/Createpage";
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => toast.success("this is the first")}></button>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
