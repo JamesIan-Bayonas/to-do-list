67e102917334bd0e412cb141f8d0292514383def
James-Ian09 <jamesianbayonas21@gmail.com>
2025-07-01 23:45:34 +0800

    added functionalities specifically in homepage navigation hyperlink

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router";
 import Homepage from "./pages/Homepage";
 import NoteDetailpage from "./pages/NoteDetailpage";
 import Createpage from ".page/Createpage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
       <button onClick={() => toast.success("this is the first")}></button>
       <Routes>
+        <Route path="/" element={<Homepage />}></Route>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/" element={<Homepage />}></Route>
