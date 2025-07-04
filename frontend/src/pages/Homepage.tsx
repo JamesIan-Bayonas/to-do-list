010a3e587d09828aed87c911f934a32cf92615fe
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-07-04 22:57:27 +0800

    implemented tests for future development purposes, via paragraphs DOM tags for tagging

---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 color="green">This is the Homepage!</h1>
+      <p>Can you see me?</p>
+    </div>
+  );
+};
+
+export default Homepage;
