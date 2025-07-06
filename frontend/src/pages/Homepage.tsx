66482ad787c25e51d64af6dcb90fadd1b63b1547
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-07-06 23:53:30 +0800

    added comment to explain the code functionalities

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -4,7 +4,7 @@ const Homepage = () => {
   return (
     <div>
       <h1 color="green">This is the Homepage!</h1>
-      <p>Can you see me?</p>
+      <p className="text-red-500">Can you see me?</p>
     </div>
   );
 };
