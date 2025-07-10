7d18aa42044f7c02c12914af64a1be975fe207ab
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-07-10 23:53:44 +0800

    added backend functionalities

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 
 const Homepage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5173/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error fetching notes");
+        console.log(error);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to fetch notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
 
   return (
     <div className="min-h-screen">
