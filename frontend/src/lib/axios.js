1ba3db9f4a72e24272b32433ea824b9c6cf3b101
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-07-17 23:46:44 +0800

    added comment for highlighting needed task

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
+
+// :: this logic seems to be at this time really confusing ill get this later! ::
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
